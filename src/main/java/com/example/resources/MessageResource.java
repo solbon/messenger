80704990b04edda328e71e550c524c091c6a08b7
Solbon Daribazaron <solbon@gmail.com>
2017-09-07 06:56:07 +0200

    Add getMessage method

--- src/main/java/com/example/resources/MessageResource.java
+++ src/main/java/com/example/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.example.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -21,5 +22,12 @@ public class MessageResource {
     @Produces(MediaType.APPLICATION_XML)
     public List<Message> getMessages() {
         return service.getAllMessages();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/{messageId}")
+    public Message getMessage(@PathParam ("messageId") Long messageId) {
+        return service.getMessage(messageId);
     }
 }
