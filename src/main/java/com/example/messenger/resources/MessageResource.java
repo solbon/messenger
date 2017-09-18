54df685ff1751e73badd577df2054c2dc10590c5
solbon <solbon@gmail.com>
2017-09-18 11:51:37 +0200

    POST, PUT and DELETE implementation

--- src/main/java/com/example/messenger/resources/MessageResource.java
+++ src/main/java/com/example/messenger/resources/MessageResource.java
@@ -3,10 +3,7 @@ package com.example.messenger.resources;
 import com.example.messenger.model.Message;
 import com.example.messenger.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -14,20 +11,38 @@ import java.util.List;
  * Created by Solbon on 2017-06-22.
  */
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService service = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return service.getAllMessages();
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
     @Path("/{messageId}")
     public Message getMessage(@PathParam ("messageId") Long messageId) {
         return service.getMessage(messageId);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam ("messageId") Long id, Message message) {
+        message.setId(id);
+        return service.updateMessage(message);
+    }
+
+    @POST
+    public Message addMessage(Message message) {
+        return service.addMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}")
+    public Message removeMessage(@PathParam("messageId") Long id) {
+        return service.removeMessage(id);
     }
 }
